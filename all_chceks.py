9aff8bbd2175d082578becb5a64ae145b6d94796
masaadotaibi <mosaed_9999@hotmail.com>
2021-06-29 19:51:24 +0300

    The function check_disk_full now returns True! And refactored some code
    The function check_disk_full were erronously previously named as check_disk_usage
    so we renamed it to the check_disk_full. The function check_disk_full was returning
    the value true if the value True if the disk is proceeding the specified limit
    through arguments, and False otherwise, and now we fixed this bug. At last, I have
    created a wrapper function for check_disk_full to check usage of only the root.

--- all_chceks.py
+++ all_chceks.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 import os
 import sys
-from disk_usage import check_disk_usage
+from disk_usage import check_disk_full
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
+
+
+def check_root_usage():
+    """Returns True if the root partition is full, False otherwise."""
+    # Chcek for at least 2 GB and 10% free
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
 
-    # Chcek for at least 2 GB and 10% free
-    if check_disk_usage(disk="/", min_gb=2, min_percent=10):
-        print("Disk full!")
+    if check_root_usage():
+        print("Root partition full!")
         sys.exit(1)
 
     print("Everything ok.")
--- disk_usage.py
+++ disk_usage.py
@@ -3,7 +3,7 @@
 import shutil
 import sys
 
-def check_disk_usage(disk, min_gb, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
     """Returns True if therse is enough free disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
@@ -11,6 +11,6 @@ def check_disk_usage(disk, min_gb, min_percent):
     # Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
     if percent_free < min_percent or gigabytes_free < min_gb:
-        return False
-    else:
         return True
+
+    return False
